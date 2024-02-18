2680c9da4647d591074951f14e802bf9399f21f6
tokimay <tokimay@gmail.com>
2024-02-18 11:54:05 +0330

    env

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-"""
-1 - start mic stream
-2 - graph(spectrum & FFT) and play
-3 - save to postgresql (DB=sound, user=python, pass=python, table=storage)
-4 - get from postgresql
-5 - graph and play
-
-tokimay@gmail.com
-"""
 import psycopg2
 import pyaudio
 import sys
